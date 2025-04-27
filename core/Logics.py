7cc625610bffe33e5f5eaf3ddacb471c969372df
WayanFWP <Piwayan1@gmail.com>
2025-04-28 01:54:50 +0700

    wanna do some thing on home so it can be seen on flow data section and the home section

--- core/Logics.py
+++ core/Logics.py
@@ -7,39 +7,99 @@ from traceback import print_exc
 class Logic:
     def __init__(self, variable):
         self.var = variable
-        self.file_path = "data/samples_10sec.csv"
-        self.fs = None # Default sampling frequency
+        # self.file_path = "data/samples_10sec.csv"
+        self.file_path = "data/samples_1minute.csv"
 
+    # data flow section
     def process_data(self):
         # step 1
-        file_path = st.sidebar.file_uploader("Upload CSV file", type=["csv"])
-        self.loadDisplayData(file_path=self.file_path)
-        # self.loadDisplayData(file_path)
-        # LOG_INFO("Load Data", self.var.dataECG, content="dataframe")
-        # self.fs = np.max(self.var.dataECG)
+        if self.var.dataECG is None:
+            file_path = st.sidebar.file_uploader("Upload CSV file", type=["csv", "txt"])
+            self.loadDisplayData(file_path)
+        # elif self.var.dataECG is not None:
+        #     loadDisplayData(self.var.dataECG)  # For testing, using a default file path
+        # hardcoded
+        # self.loadDisplayData(file_path=self.file_path)
 
-        if self.var is not None:
-            st.write("Performing DFT on the data...")
-            # Perform DFT on the raw ECG data
-            self.loadDFT(self.var.dataECG, absolute=True, transformType="DFT")
-
-        
+        # Perform DFT on the raw ECG data
+        # st.write("Performing DFT on the data...")
+        # self.loadDFT(self.var.dataECG, absolute=True, transformType="DFT")
         if self.var.dataECG is not None:
+            fs = 2 * np.max(np.abs(self.var.dataECG))  # Default sampling frequency using fs = 2 * max absolute value            
+            duration = len(self.var.dataECG) * 0.01  # Duration in seconds why using 0.01? because the sample interval is 0.01 seconds
+            st.write(f"fs: {fs},Duration: {duration:.2f} seconds, data: {len(self.var.dataECG)}")
             st.subheader("Plot filtered data...")
-            fc_low = st.slider("Low Cutoff for LPF", min_value=1, max_value=500, value=100)
-            orde = st.slider("Filter Order", min_value=1, max_value=10, value=4)
-            fs_lowpass = 2*np.max(self.var.dataECG)  # Sampling frequency
 
-            # st.write(f"Parameters: fc_low={fc_low}, orde={orde}, fs={fs}")
+            filter_param = {
+                "fc_l": st.text_input("Low Cutoff Frequency (Hz)", value="1"),
+                "fc_h": st.text_input("High Cutoff Frequency (Hz)", value="0.5"),
+                "orde_filter": st.number_input("Orde Frequency", max_value=10, min_value=1, value=2),
+            }
+            try:
+                parsed_param = {
+                    key: float(value) if key != "orde_filter" else int(value) for key, value in filter_param.items()
+                }
+            except ValueError:
+                st.error("Please enter two numeric values separated by a space for each threshold.")
 
-            amplitude = self.var.dataECG - np.mean(self.var.dataECG)
-            self.applyFilter(filter_type="LPF", data_input=amplitude, absolute=True, fcl=fc_low, fch = 1, orde=1, fs=fs_lowpass)       
-            # self.applyFilter(filter_type="LPF", data_input=self.var.dft, absolute=False, fcl=fc_low, fch = 1, orde=1, fs=self.fs)       
+            amplitude = self.var.dataECG
+            st.write(f"fc_l: {parsed_param['fc_l']}, fc_h: {parsed_param['fc_h']}, orde: {parsed_param['orde_filter']}")
+            # self.applyFilter(filter_type="BPF", data_input=amplitude, absolute=False, fcl=fc_l, fch = fc_h, orde=orde_LPF, frequencySampling=fs)
+            self.filtered_data = self.applyFilter(filter_type="LPF", data_input=amplitude, absolute=False, fcl=parsed_param["fc_l"], fch=parsed_param["fc_h"], orde=parsed_param["orde_filter"], frequencySampling=fs)     
 
-       
+        if self.var.filtered_data is not None:
+            # Input thresholds for ECG peak detection
+            thresholds = {
+                "P": st.text_input("Threshold for P peak detection (min max)", value="0.11 0.2"),
+                "Q": st.text_input("Threshold for Q peak detection (min max)", value="-0.28 -0.22"),
+                "R": st.text_input("Threshold for R peak detection (min max)", value="1 1.5"),
+                "S": st.text_input("Threshold for S peak detection (min max)", value="-0.9 -0.50"),
+                "T": st.text_input("Threshold for T peak detection (min max)", value="0.23 0.38"),
+            }
+
+            try:
+                # Parse thresholds
+                parsed_thresholds = {
+                    key: list(map(float, value.split())) for key, value in thresholds.items()
+                }
+                # Perform ECG segmentation
+                segmentation = segment_ecg(
+                    self.var.filtered_data,
+                    threshold_p=parsed_thresholds["P"],
+                    threshold_q=parsed_thresholds["Q"],
+                    threshold_r=parsed_thresholds["R"],
+                    threshold_s=parsed_thresholds["S"],
+                    threshold_t=parsed_thresholds["T"],
+                )
+            except ValueError:
+                st.error("Please enter two numeric values separated by a space for each threshold.")
+
+            import matplotlib.pyplot as plt
+                
+            plt.figure(figsize=(10, 4))
+            plt.plot(self.var.filtered_data)
+
+            for komponen in segmentation:
+                plt.scatter(segmentation[komponen]['lokasi'], segmentation[komponen]['nilai'], label=komponen)
+
+            for peak_type, peak_data in segmentation.items():
+                if 'lokasi' in peak_data:
+                    heart_rate = calculate_heart_rate(peak_data['lokasi'], duration)
+                    st.write(f"Signal detected {peak_type} : {heart_rate} in second")
+            # st.write(f"P: {segmentation}, Q: {}, R: {}, S: {}, T: {}")
+            plt.legend()
+            st.pyplot(plt)   
+            # Use st.pyplot to display the plot in Streamlit
+            if 'R' in segmentation and segmentation['R']['lokasi']:
+                heart_rate =calculate_heart_rate(segmentation['R']['lokasi'], duration)
+                st.write(f"Heart Rate: {heart_rate} bpm")
+            else: st.write("no R peaks found in the filtered data.")
+
+                
         else: st.write("Please upload data first on the 'Data' page.")
 
 
+    # load section
     def loadDisplayData(self, file_path=None, data_title="Raw ECG"):
         # Read the CSV file and extract the ECG column
         if file_path is not None:
@@ -57,7 +117,6 @@ class Logic:
                 st.error(f"Error reading the file: {e}")
         
         elif self.var.dataECG is not None: plotLine(f"{data_title} Data", self.var.dataECG)
-        # else: st.error("Please upload a file first.")
 
     def loadDFT(self, data_input, absolute=False, transformType="DFT"):
         if data_input is None:
@@ -78,10 +137,11 @@ class Logic:
 
         except Exception as e:
             st.error(f"Error during {transformType}: {e}")
+            print_exc()
             LOG_INFO("Error", self.var.dft, content="dataframe")
             self.var.dft = None
 
-    def applyFilter(self, filter_type="LPF", data_input=None, absolute=False,fcl=None, fch=None, orde=None, fs=None):
+    def applyFilter(self, filter_type="LPF", data_input=None, absolute=False,fcl=None, fch=None, orde=None, frequencySampling=None):
         if data_input is None:
             st.warning("No data to filter.")
             return
@@ -89,43 +149,45 @@ class Logic:
         import pandas as pd
         if isinstance(data_input, pd.DataFrame):
             data_input = data_input.values.flatten()  # Convert DataFrame to 1D array
-
-        # Validasi parameter
-        if fcl is None or fcl <= 0:
-            st.error("Invalid Low Cutoff Frequency (fcl). It must be greater than 0.")
-            return
-        if orde is None or orde <= 0:
-            st.error("Invalid Filter Order (orde). It must be greater than 0.")
-            return
-        if fs is None or fs <= 0:
-            st.error("Invalid Sampling Frequency (fs). It must be greater than 0.")
+        
+        if fch is None or orde is None or frequencySampling is None:
+            st.error("Cutoff frequency, filter order, and sampling frequency must be specified.")
             return
 
+        if orde <= 0:
+            st.error("Filter order must be greater than 0.")
+            return
+
+        if frequencySampling <= 0:
+            st.error("Sampling frequency must be greater than 0.")
+            return
         try:
             if filter_type == "LPF":
-                h_i = LPF(fcl, orde, fs)
-                filtered_data = forward_filter(h_i, data_input)
-                filtered_data = backward_filter(h_i, filtered_data)
-                
-                proceed = DFT(filtered_data)
-                plotDFT("LPF Result", proceed, absolute)
+                b, a = LPF(fcl, orde, frequencySampling)
+                filtered_data = forward_backward_filter(b, a, data_input)
+                plotLine("Filtered Data(Lowpass)", filtered_data)
+                if absolute:
+                    hasil_data = DFT(filtered_data)
+                    plotDFT("Filtered Data(Lowpass)", hasil_data, absolute=True)
+
             elif filter_type == "HPF":
-                h_i = HPF(fcl, orde, fs)
-                filtered_data = forward_filter(h_i, data_input)
-                filtered_data = backward_filter(h_i, filtered_data)
+                b, a = HPF(fch, orde, frequencySampling)
+                filtered_data = forward_backward_filter(b, a, data_input)
+                plotLine("Filtered Data(Highpass)", filtered_data)
 
-                proceed = DFT(filtered_data)
-                plotDFT("HPF Result", proceed, absolute)
             elif filter_type == "BPF":
-                h_i = BPF(fcl, fch, orde, fs)
-                filtered_data = forward_filter(h_i, data_input)
-                filtered_data = backward_filter(h_i, filtered_data)
+                b, a = BPF(fcl, fch, orde, frequencySampling)
+                filtered_data = forward_backward_filter(b, a, data_input)
+                plotLine("Filtered Data(Bandpass)", filtered_data)
 
-                proceed = DFT(filtered_data)
-                plotDFT("BPF Result", proceed, absolute)               
+            elif filter_type == "BSF":
+                b, a = BSF(fcl, fch, orde, frequencySampling)
+                filtered_data = forward_backward_filter(b, a, data_input)
+                plotLine("Filtered Data(Bandstop)", filtered_data)
 
             self.var.filtered_data = np.real(filtered_data)
-
+        except ValueError as e:
+            st.error(f"Filter error: {e}")
         except Exception as e:
             print_exc()
             st.error(f"Error during {filter_type}: {e}")
--- core/Variables.py
+++ core/Variables.py
@@ -2,12 +2,6 @@ import streamlit as st
 
 class Variable:
     def __init__(self):
-        self.amplitude = 1  # Amplitude in mV
-        self.GLOBAL_fcl = st.sidebar.slider("Low Cutoff Frequency", min_value=10,max_value=100, value=10)  # Low cutoff frequency in Hz
-        self.GLOBAL_fch = st.sidebar.slider("High Cutoff Frequency", min_value=1, max_value=50, value=1) # High cutoff frequency in Hz
-        self.GLOBAL_orde = st.sidebar.slider("Filter Order", min_value=1, max_value=100, value=1)
-        self.GLOBAL_MAV = 30 # Moving average window size
-
         self._dataECG = None
         self._dft = None
         self._filtered_data = None
@@ -18,15 +12,17 @@ class Variable:
         st.session_state.setdefault('filtered_data', None)
 
     # Getter and Setter for each variable
-    @property
+    @property # dataECG
     def dataECG(self): return self._dataECG
     @dataECG.setter
     def dataECG(self, value): self._dataECG = value; st.session_state.dataECG = value
-    @property
+
+    @property # dft
     def dft(self): return self._dft
     @dft.setter
     def dft(self, value): self._dft = value; st.session_state.dft = value
-    @property
+
+    @property # filtered_data
     def filtered_data(self): return self._filtered_data
     @filtered_data.setter
     def filtered_data(self, value): self._filtered_data = value; st.session_state.filtered_data = value
