0cdb6fe6ca94f72375725bfd09c8659d507984f1
WayanFWP <Piwayan1@gmail.com>
2025-04-30 06:16:44 +0700

    make a confic for 10 sec 150 hz cutoff

--- core/config.py
+++ core/config.py
@@ -1,17 +1,33 @@
 # Default filter parameters
 DEFAULT_FILTER_PARAMS = {
-    "fc_l": 1.0,
+    "fc_l": 100.0,
     "fc_h": 0.5,
     "orde_filter": 2,
 }
 
-# Default thresholds for peak detection
+# Default thresholds for peak detection with sample 1min
+# DEFAULT_THRESHOLDS = {
+#     "P": "0.11 0.2",
+#     "Q": "-0.28 -0.22",
+#     "R": "1 1.5",
+#     "S": "-0.9 -0.50",
+#     "T": "0.23 0.38",
+# }
+# Default thresholds for peak detection with sample 10sec 100
 DEFAULT_THRESHOLDS = {
-    "P": "0.11 0.2",
-    "Q": "-0.28 -0.22",
-    "R": "1 1.5",
-    "S": "-0.9 -0.50",
-    "T": "0.23 0.38",
+    "P": "15 21",
+    "Q": "-35 -25",
+    "R": "115 140",
+    "S": "-85 -60",
+    "T": "32 43",
+}
+# Default thresholds for peak detection with sample 10sec
+DEFAULT_THRESHOLDS = {
+    "P": "5 9",
+    "Q": "-35 -25",
+    "R": "38 45",
+    "S": "-85 -60",
+    "T": "15 18",
 }
 
 DEFAULT_SAMPLE_INTERVAL = 0.01 #seconds
