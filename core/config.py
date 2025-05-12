5946a4ca9120499592790849f729620cba6fd12e
WayanFWP <Piwayan1@gmail.com>
2025-05-12 22:00:35 +0700

    remove unnecessary config

--- core/config.py
+++ core/config.py
@@ -7,12 +7,12 @@
 #     "T": "0.23 0.38",
 # }
 # # Default thresholds for peak detection with sample 10sec 150
-DEFAULT_THRESHOLDS = {
-    "P": "15 21",
-    "Q": "-35 -25",
-    "R": "115 140",
-    "S": "-85 -60",
-    "T": "32 43",
-}
+# DEFAULT_THRESHOLDS = {
+#     "P": "15 21",
+#     "Q": "-35 -25",
+#     "R": "115 140",
+#     "S": "-85 -60",
+#     "T": "32 43",
+# }
 
 DEFAULT_SAMPLE_INTERVAL = 0.01 #seconds
